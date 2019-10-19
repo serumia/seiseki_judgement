db80db580f2c66a8f8adeea488a96e035422f4af
serumia <serumia0910@yahoo.co.jp>
2019-10-19 15:13:58 +0900

    製作途中

--- app/src/main/java/com/example/seiseki_judgement/MainActivity.kt
+++ app/src/main/java/com/example/seiseki_judgement/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.seiseki_judgement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sub1.setOnClickListener{
+            val intent = Intent(this, sub1_main::class.java)
+            startActivity(intent)
+        }
     }
 }
