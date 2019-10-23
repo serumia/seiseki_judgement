e9e75767df92f2c5e85486d25a75778348540777
serumia <serumia0910@yahoo.co.jp>
2019-10-24 08:46:32 +0900

    fee

--- app/src/main/java/com/example/seiseki_judgement/MainActivity.kt
+++ app/src/main/java/com/example/seiseki_judgement/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_sub_1.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +15,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-
     }
 
-fun sub1tapped(view: View?){
-    val intent = Intent(this, sub1Activity::class.java)
-    startActivity(intent)
+    fun sub1tapped(view: View?) {
+        val intent = Intent(this, sub1Activity::class.java)
+        startActivity(intent)
+    }
+
+    fun goukei(view: View) {
+        val x: Array<EditText> = arrayOf(tenn1, tenn2, tenn3, tenn4)
+        val sum = x[0] + x[1] + x[2] + x[3]
+        TextView1.text = sum.toString()
+    }
+
+    private fun toString(sum: Unit) {}
+    private operator fun Any.plus(EditText: EditText) {
+    }
 }
-}
+
