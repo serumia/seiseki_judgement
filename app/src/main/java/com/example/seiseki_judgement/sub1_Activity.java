63054518ee2a33bf7a78d155b4f5468109236213
serumia <serumia0910@yahoo.co.jp>
2019-10-25 17:39:45 +0900

    ooooooo

--- app/src/main/java/com/example/seiseki_judgement/sub1_Activity.java
+++
@@ -1,7 +0,0 @@
-package com.example.seiseki_judgement;
-
-import android.app.Activity;
-
-public class sub1_Activity extends Activity {
-
-}
---
+++ app/src/main/java/com/example/seiseki_judgement/sub1_Activity.kt
@@ -0,0 +1,5 @@
+package com.example.seiseki_judgement
+
+import android.app.Activity
+
+class sub1_Activity : Activity()
